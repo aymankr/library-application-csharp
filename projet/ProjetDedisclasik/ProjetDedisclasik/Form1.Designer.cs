98263f15a894726b5f5d1fcbafe4e09f46c44268
Ayman Kr <kr.ayman@hotmail.com>
2021-06-04 14:28:51 +0200

    ajout abonné

--- projet/ProjetDedisclasik/ProjetDedisclasik/Form1.Designer.cs
+++ projet/ProjetDedisclasik/ProjetDedisclasik/Form1.Designer.cs
@@ -29,13 +29,156 @@ namespace ProjetDedisclasik
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.mdp = new System.Windows.Forms.TextBox();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ajout = new System.Windows.Forms.Button();
+            this.listAbonnes = new System.Windows.Forms.ListBox();
+            this.login = new System.Windows.Forms.TextBox();
+            this.loginAbo = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pays = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(101, 59);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(100, 20);
+            this.nom.TabIndex = 0;
+            // 
+            // mdp
+            // 
+            this.mdp.Location = new System.Drawing.Point(101, 137);
+            this.mdp.Name = "mdp";
+            this.mdp.Size = new System.Drawing.Size(100, 20);
+            this.mdp.TabIndex = 1;
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(101, 96);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(100, 20);
+            this.prenom.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nom :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prénom :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mot de passe : ";
+            // 
+            // ajout
+            // 
+            this.ajout.Location = new System.Drawing.Point(73, 303);
+            this.ajout.Name = "ajout";
+            this.ajout.Size = new System.Drawing.Size(128, 48);
+            this.ajout.TabIndex = 6;
+            this.ajout.Text = "Ajouter abonné";
+            this.ajout.UseVisualStyleBackColor = true;
+            this.ajout.Click += new System.EventHandler(this.ajout_Click);
+            // 
+            // listAbonnes
+            // 
+            this.listAbonnes.FormattingEnabled = true;
+            this.listAbonnes.Location = new System.Drawing.Point(384, 62);
+            this.listAbonnes.Name = "listAbonnes";
+            this.listAbonnes.Size = new System.Drawing.Size(287, 303);
+            this.listAbonnes.TabIndex = 7;
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(101, 181);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 8;
+            // 
+            // loginAbo
+            // 
+            this.loginAbo.AutoSize = true;
+            this.loginAbo.Location = new System.Drawing.Point(39, 184);
+            this.loginAbo.Name = "loginAbo";
+            this.loginAbo.Size = new System.Drawing.Size(39, 13);
+            this.loginAbo.TabIndex = 9;
+            this.loginAbo.Text = "Login :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 246);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Pays :";
+            // 
+            // pays
+            // 
+            this.pays.Location = new System.Drawing.Point(101, 243);
+            this.pays.Name = "pays";
+            this.pays.Size = new System.Drawing.Size(100, 20);
+            this.pays.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pays);
+            this.Controls.Add(this.loginAbo);
+            this.Controls.Add(this.login);
+            this.Controls.Add(this.listAbonnes);
+            this.Controls.Add(this.ajout);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.prenom);
+            this.Controls.Add(this.mdp);
+            this.Controls.Add(this.nom);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.TextBox mdp;
+        private System.Windows.Forms.TextBox prenom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button ajout;
+        private System.Windows.Forms.ListBox listAbonnes;
+        private System.Windows.Forms.TextBox login;
+        private System.Windows.Forms.Label loginAbo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox pays;
     }
 }
 
--- projet/ProjetDedisclasik/ProjetDedisclasik/Form1.cs
+++ projet/ProjetDedisclasik/ProjetDedisclasik/Form1.cs
@@ -12,9 +12,46 @@ namespace ProjetDedisclasik
 {
     public partial class Form1 : Form
     {
+        MusiquePT2_NEntities musique;
+
+
         public Form1()
         {
             InitializeComponent();
+            musique = new MusiquePT2_NEntities();
+            chargeListAbonnes();
+        }
+
+        private void ajout_Click(object sender, EventArgs e)
+        {
+            ABONNÉS abonne = new ABONNÉS();
+
+            String nomAbonne = nom.Text;
+            String prenomAbonne = prenom.Text;
+            String loginAbonne = loginAbo.Text;
+            String mdpAbonne = mdp.Text;
+            String paysAbonne = pays.Text;
+
+            abonne.NOM_ABONNÉ = nomAbonne;
+            abonne.PRÉNOM_ABONNÉ = prenomAbonne;
+            abonne.LOGIN_ABONNÉ = loginAbonne;
+            abonne.PASSWORD_ABONNÉ = mdpAbonne;
+
+            musique.ABONNÉS.Add(abonne);
+            musique.SaveChanges();
+            chargeListAbonnes();
+        }
+
+        private void chargeListAbonnes()
+        {
+            var abonnes = (from a in musique.ABONNÉS
+                           orderby a.NOM_ABONNÉ
+                           select a).ToList();
+            listAbonnes.Items.Clear();
+            foreach (ABONNÉS a in abonnes)
+            {
+                listAbonnes.Items.Add(a.NOM_ABONNÉ);
+            }
         }
     }
 }
