b9a3463b7721ec3296018f891a0a49834cbef6b5
Ayman Kr <kr.ayman@hotmail.com>
2021-06-04 16:12:33 +0200

    inscription et connexion d'un abonnée opérationnel

---
+++ projet/ProjetDedisclasik/ProjetDedisclasik/Connexion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetDedisclasik
+{
+    public partial class Connexion : Form
+    {
+        MusiquePT2_NEntities musique;
+        public Connexion()
+        {
+            InitializeComponent();
+            musique = new MusiquePT2_NEntities();
+        }
+
+        private void inscription_Click(object sender, EventArgs e)
+        {
+            Inscription inscription = new Inscription(musique);
+            inscription.ShowDialog();
+        }
+
+        private void connexion_Click(object sender, EventArgs e)
+        {
+            if (connexionValide()) MessageBox.Show("Connexion réussie.");
+        }
+
+        private bool connexionValide()
+        {
+            bool valide = false;
+            String login = loginConnexion.Text;
+            String mdp = mdpConnexion.Text;
+            var abonnes = (from a in musique.ABONNÉS
+                           select a).ToList();
+            foreach (ABONNÉS a in abonnes)
+            {
+                if (login.Equals(a.LOGIN_ABONNÉ.Trim()) && mdp.Equals(a.PASSWORD_ABONNÉ.Trim())) valide = true;
+            }
+
+            return valide;
+        }
+    }
+}
---
+++ projet/ProjetDedisclasik/ProjetDedisclasik/Inscription.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetDedisclasik
+{
+    public partial class Inscription : Form
+    {
+        MusiquePT2_NEntities musique;
+
+        public Inscription(MusiquePT2_NEntities musique)
+        {
+            InitializeComponent();
+            this.musique = musique;
+            chargeListAbonnes();
+        }
+
+        private void ajout_Click(object sender, EventArgs e)
+        {
+            ABONNÉS abonne = new ABONNÉS();
+
+            String nomAbonne = nom.Text;
+            String prenomAbonne = prenom.Text;
+            String loginAbonne = login.Text;
+            String mdpAbonne = mdp.Text;
+
+            abonne.NOM_ABONNÉ = nomAbonne;
+            abonne.PRÉNOM_ABONNÉ = prenomAbonne;
+            abonne.LOGIN_ABONNÉ = loginAbonne;
+            abonne.PASSWORD_ABONNÉ = mdpAbonne;
+            Console.WriteLine(abonne.LOGIN_ABONNÉ);
+
+            if (nomAbonne.Equals("") || prenomAbonne.Equals("") || loginAbonne.Equals(""))
+            {
+                MessageBox.Show("Veuillez saisir les informations dans les champs spécifiés.");
+            }
+            else if (loginExiste(loginAbonne))
+            {
+                MessageBox.Show("Le login existe déjà, réessayez.");
+            }
+            else
+            {
+                musique.ABONNÉS.Add(abonne);
+                musique.SaveChanges();
+                chargeListAbonnes();
+                Close();
+            }
+        }
+
+        private void chargeListAbonnes()
+        {
+            var abonnes = (from a in musique.ABONNÉS
+                           orderby a.NOM_ABONNÉ
+                           select a).ToList();
+            listAbonnes.Items.Clear();
+            foreach (ABONNÉS a in abonnes)
+            {
+                String noms = a.PRÉNOM_ABONNÉ.Trim() + " " +  a.NOM_ABONNÉ;
+                listAbonnes.Items.Add(noms);
+            }
+        }
+
+        private bool loginExiste(String chaine)
+        {
+            bool existe = false;
+            var abonnes = (from a in musique.ABONNÉS
+                           select a).ToList();
+           
+            foreach (ABONNÉS a in abonnes)
+            {
+                if (a.LOGIN_ABONNÉ.Trim().Equals(chaine)) existe = true;
+            }
+
+            return existe;
+        }
+
+
+    }
+}
