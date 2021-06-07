9ff5766b89b492d15612eb03256752c0dce848e5
Ayman Kr <kr.ayman@hotmail.com>
2021-06-07 14:15:42 +0200

    US5 terminé

--- projet/Dedisclasik/Dedisclasik/Connexion.cs
+++ projet/Dedisclasik/Dedisclasik/Connexion.cs
@@ -29,18 +29,27 @@ namespace Dedisclasik
 
         private void connexion_Click(object sender, EventArgs e)
         {
-            String login = loginConnexion.Text;
-            String mdp = mdpConnexion.Text;
-            if (connexionValide(login, mdp))
+            string login = loginConnexion.Text;
+            string mdp = mdpConnexion.Text;
+            if (connexionAdminValide())
+            {
+                Administrateur administrateur = new Administrateur();
+                administrateur.ShowDialog();
+            }
+            else if (connexionValide(login, mdp))
             {
                 id_abonné = ABONNÉS.RechercheAbonne(login);
 
                 RechercheAlbumEtEmprunt emprunt = new RechercheAlbumEtEmprunt();
                 emprunt.ShowDialog();
             }
+            else
+            {
+                MessageBox.Show("Connexion échouée.");
+            }
         }
 
-        private bool connexionValide(String login, String mdp)
+        private bool connexionValide(string login, string mdp)
         {
             bool valide = false;
             var abonnes = (from a in Outils.musique.ABONNÉS
@@ -50,6 +59,16 @@ namespace Dedisclasik
                 if (login.Equals(a.LOGIN_ABONNÉ.Trim()) && mdp.Equals(a.PASSWORD_ABONNÉ.Trim())) valide = true;
             }
 
+            return valide;
+        }
+
+        private bool connexionAdminValide()
+        {
+            string loginAdmin = "dedisk";
+            string mdpAdmin = "admin";
+            bool valide = false;
+
+            if (loginConnexion.Text.Equals(loginAdmin) && mdpConnexion.Text.Equals(mdpAdmin)) valide = true;
             return valide;
         }
     }
