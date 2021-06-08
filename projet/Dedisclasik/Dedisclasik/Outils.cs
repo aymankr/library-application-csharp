ed011043c110d48f4e76de761db7df0f59acd181
Ayman Kr <kr.ayman@hotmail.com>
2021-06-08 14:27:29 +0200

    amélioration affichage gridview

--- projet/Dedisclasik/Dedisclasik/Outils.cs
+++ projet/Dedisclasik/Dedisclasik/Outils.cs
@@ -20,5 +20,20 @@ namespace Dedisclasik
             }
             return dejaProlongé;
         }
+
+        public static void chargerDataGrid(int nbColonnes, string[] attributs, System.Windows.Forms.DataGridView dg)
+        {
+            dg.Rows.Clear();
+            dg.Columns.Clear();
+            dg.ColumnCount = nbColonnes;
+
+            int i = 0;
+            foreach(string s in attributs)
+            {
+                dg.Columns[i].Name = s;
+                dg.Columns[i].Width = dg.Width / nbColonnes;
+                i++;
+            }
+        }
     }
 }
