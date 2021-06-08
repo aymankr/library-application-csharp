4a520badb13a80b85ce05af4f126c0f691468b80
dgavlak <damien.gavlak@etu.u-bordeaux.fr>
2021-06-08 18:06:31 +0200

    problemeRechercheTropLongue

--- projet/Dedisclasik/Dedisclasik/RechercheAlbumEtEmprunt.cs
+++ projet/Dedisclasik/Dedisclasik/RechercheAlbumEtEmprunt.cs
@@ -36,13 +36,30 @@ namespace Dedisclasik
         private void recherche_TextChanged(object sender, EventArgs e)
         {
             album.Items.Clear();
-            pagesAlbums.Rows.Clear();
-            pagesAlbums.Columns.Clear();
-            foreach (ALBUMS al in Outils.musique.ALBUMS)
+            foreach (ALBUMS al in ABONNÉS.RechercheTitre(recherche.Text))
             {
                 album.Items.Add(al);
                 // pagesAlbums.Rows.Add(al.ToString());
             }
+
+            /*album.Items.Clear();
+            List<string> emprunter = new List<string>();
+            foreach (EMPRUNTER emp in Outils.musique.EMPRUNTER)
+            {
+                emprunter.Add(emp.ALBUMS.TITRE_ALBUM);
+            }
+            List<String> titres = ABONNÉS.RechercheTitre(recherche.Text);
+            foreach (String t in titres)
+            {
+                if (emprunter.Contains(t))
+                {
+                    album.Items.Add(t.Trim() + " -> Déjà emprunté");
+                }
+                else
+                {
+                    album.Items.Add(t);
+                }
+            }*/
         }
 
         private void emprunter_Click(object sender, EventArgs e)
