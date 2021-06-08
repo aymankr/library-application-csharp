832751b371ff8b5bdb3aec49690cb40da8420f19
Ayman Kr <kr.ayman@hotmail.com>
2021-06-08 11:35:14 +0200

    IHM sans tests

--- projet/Dedisclasik/Dedisclasik/Administrateur.Designer.cs
+++ projet/Dedisclasik/Dedisclasik/Administrateur.Designer.cs
@@ -33,83 +33,120 @@ namespace Dedisclasik
             this.purger = new System.Windows.Forms.Button();
             this.empruntRetard = new System.Windows.Forms.Button();
             this.empruntMeilleurs = new System.Windows.Forms.Button();
-            this.listInfos = new System.Windows.Forms.ListBox();
             this.albumsNonEmprunts = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.deconnect = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // empruntProlong
             // 
-            this.empruntProlong.Location = new System.Drawing.Point(59, 93);
+            this.empruntProlong.Location = new System.Drawing.Point(12, 490);
             this.empruntProlong.Name = "empruntProlong";
-            this.empruntProlong.Size = new System.Drawing.Size(154, 33);
+            this.empruntProlong.Size = new System.Drawing.Size(154, 49);
             this.empruntProlong.TabIndex = 0;
-            this.empruntProlong.Text = "EMPRUNTS PROLONGES";
+            this.empruntProlong.Text = "EMPRUNTS PROLONGÉS";
             this.empruntProlong.UseVisualStyleBackColor = true;
             this.empruntProlong.Click += new System.EventHandler(this.empruntProlong_Click);
             // 
             // purger
             // 
             this.purger.BackColor = System.Drawing.Color.LightCoral;
-            this.purger.Location = new System.Drawing.Point(59, 276);
+            this.purger.Location = new System.Drawing.Point(738, 490);
             this.purger.Name = "purger";
-            this.purger.Size = new System.Drawing.Size(154, 51);
+            this.purger.Size = new System.Drawing.Size(154, 49);
             this.purger.TabIndex = 1;
-            this.purger.Text = "PURGER ABONNES N\'AYANT PAS EMPRUNTE DEPUIS UN AN";
+            this.purger.Text = "PURGER ABONNÉS N\'AYANT PAS EMPRUNTÉ DEPUIS UN AN";
             this.purger.UseVisualStyleBackColor = false;
             this.purger.Click += new System.EventHandler(this.purger_Click);
             // 
             // empruntRetard
             // 
-            this.empruntRetard.Location = new System.Drawing.Point(59, 132);
+            this.empruntRetard.Location = new System.Drawing.Point(172, 490);
             this.empruntRetard.Name = "empruntRetard";
-            this.empruntRetard.Size = new System.Drawing.Size(154, 37);
+            this.empruntRetard.Size = new System.Drawing.Size(154, 49);
             this.empruntRetard.TabIndex = 2;
-            this.empruntRetard.Text = "EMPRUNTS EN RETARD DE 10J";
+            this.empruntRetard.Text = "EMPRUNTS EN RETARD DE 10 JOURS";
             this.empruntRetard.UseVisualStyleBackColor = true;
             this.empruntRetard.Click += new System.EventHandler(this.empruntRetard_Click);
             // 
             // empruntMeilleurs
             // 
-            this.empruntMeilleurs.Location = new System.Drawing.Point(59, 175);
+            this.empruntMeilleurs.Location = new System.Drawing.Point(332, 490);
             this.empruntMeilleurs.Name = "empruntMeilleurs";
-            this.empruntMeilleurs.Size = new System.Drawing.Size(154, 40);
+            this.empruntMeilleurs.Size = new System.Drawing.Size(154, 49);
             this.empruntMeilleurs.TabIndex = 3;
-            this.empruntMeilleurs.Text = "10 ALBUMS LES PLUS EMPRUNTES DE L\'ANNEE";
+            this.empruntMeilleurs.Text = "10 ALBUMS LES PLUS EMPRUNTÉS DE L\'ANNEE";
             this.empruntMeilleurs.UseVisualStyleBackColor = true;
             this.empruntMeilleurs.Click += new System.EventHandler(this.empruntMeilleurs_Click);
             // 
-            // listInfos
-            // 
-            this.listInfos.FormattingEnabled = true;
-            this.listInfos.Location = new System.Drawing.Point(298, 55);
-            this.listInfos.Name = "listInfos";
-            this.listInfos.Size = new System.Drawing.Size(441, 316);
-            this.listInfos.TabIndex = 4;
-            // 
             // albumsNonEmprunts
             // 
-            this.albumsNonEmprunts.Location = new System.Drawing.Point(59, 221);
+            this.albumsNonEmprunts.Location = new System.Drawing.Point(492, 490);
             this.albumsNonEmprunts.Name = "albumsNonEmprunts";
             this.albumsNonEmprunts.Size = new System.Drawing.Size(154, 49);
             this.albumsNonEmprunts.TabIndex = 5;
-            this.albumsNonEmprunts.Text = "ALBUMS NON EMPRUNTES DEPUIS UN AN";
+            this.albumsNonEmprunts.Text = "ALBUMS NON EMPRUNTÉS DEPUIS UN AN";
             this.albumsNonEmprunts.UseVisualStyleBackColor = true;
             this.albumsNonEmprunts.Click += new System.EventHandler(this.albumsNonEmprunts_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(880, 432);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(330, 36);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Gestion administrateur";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // deconnect
+            // 
+            this.deconnect.BackColor = System.Drawing.SystemColors.Control;
+            this.deconnect.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.deconnect.FlatAppearance.BorderSize = 0;
+            this.deconnect.Location = new System.Drawing.Point(783, 12);
+            this.deconnect.Name = "deconnect";
+            this.deconnect.Size = new System.Drawing.Size(109, 34);
+            this.deconnect.TabIndex = 9;
+            this.deconnect.Text = "Se déconnecter";
+            this.deconnect.UseVisualStyleBackColor = false;
+            this.deconnect.Click += new System.EventHandler(this.deconnect_Click);
             // 
             // Administrateur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 416);
+            this.ClientSize = new System.Drawing.Size(904, 551);
+            this.Controls.Add(this.deconnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.albumsNonEmprunts);
-            this.Controls.Add(this.listInfos);
             this.Controls.Add(this.empruntMeilleurs);
             this.Controls.Add(this.empruntRetard);
             this.Controls.Add(this.purger);
             this.Controls.Add(this.empruntProlong);
             this.Name = "Administrateur";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Administrateur";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -119,7 +156,9 @@ namespace Dedisclasik
         private System.Windows.Forms.Button purger;
         private System.Windows.Forms.Button empruntRetard;
         private System.Windows.Forms.Button empruntMeilleurs;
-        private System.Windows.Forms.ListBox listInfos;
         private System.Windows.Forms.Button albumsNonEmprunts;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button deconnect;
     }
 }
--- projet/Dedisclasik/Dedisclasik/Administrateur.cs
+++ projet/Dedisclasik/Dedisclasik/Administrateur.cs
@@ -20,22 +20,25 @@ namespace Dedisclasik
 
         private void empruntProlong_Click(object sender, EventArgs e)
         {
+            chargerDataGrid(3);
+
             // US4 : abonnés ayant prolongé leur emprunt
-            listInfos.Items.Clear();
             var id_album = from al in Outils.musique.ALBUMS
                            select al.CODE_ALBUM;
             foreach (EMPRUNTER emp in Outils.musique.EMPRUNTER)
             {
                 if (Outils.dejaProlongé(emp))
                 {
-                    listInfos.Items.Add(emp.ALBUMS.TITRE_ALBUM.Trim() + " -> " + emp.ABONNÉS.LOGIN_ABONNÉ);
+                    string[] row = new string[] { emp.ALBUMS.TITRE_ALBUM, emp.ABONNÉS.NOM_ABONNÉ, emp.ABONNÉS.PRÉNOM_ABONNÉ };
+                    dataGridView1.Rows.Add(row);
                 }
             }
+            afficherMessageVide();
         }
 
         private void empruntRetard_Click(object sender, EventArgs e)
         {
-            listInfos.Items.Clear();
+            chargerDataGrid(2);
             DateTime dateNow = DateTime.Now;
 
             // US5 : abonnés ayant emprunts non rendu depuis plus de 10j
@@ -44,13 +47,15 @@ namespace Dedisclasik
                 .Where(a => (int)(dateNow - a.DATE_RETOUR_ATTENDUE).TotalDays >= 10).Select(a => a.ABONNÉS);
             foreach (ABONNÉS a in emprunteurs)
             {
-                listInfos.Items.Add(a.NOM_ABONNÉ);
+                string[] row = new string[] { a.NOM_ABONNÉ, a.PRÉNOM_ABONNÉ };
+                dataGridView1.Rows.Add(row);
             }
+            afficherMessageVide();
         }
 
         private void empruntMeilleurs_Click(object sender, EventArgs e)
         {
-            listInfos.Items.Clear();
+            chargerDataGrid(1);
             DateTime dateNow = DateTime.Now;
 
             // US7 : les 10 plus empruntés de l'année
@@ -58,17 +63,14 @@ namespace Dedisclasik
                 .Where(a => a.DATE_EMPRUNT.Year == dateNow.Year)
                 .OrderByDescending(a => a.ALBUMS.EMPRUNTER.Count).Take(10).ToList()
                 .Select(a => a.ALBUMS);
-            foreach (ALBUMS a in emprunteurs)
-            {
-                listInfos.Items.Add(a.TITRE_ALBUM);
-            }
+            foreach (ALBUMS a in emprunteurs) dataGridView1.Rows.Add(a.TITRE_ALBUM);
+            afficherMessageVide();
         }
 
         private void purger_Click(object sender, EventArgs e)
         {
-            listInfos.Items.Clear();
             DateTime dateNow = DateTime.Now;
-            //DateTime dernierEmprunt = musique.EMPRUNTER.OrderByDescending(a => a.DATE_EMPRUNT).Select(a => a.DATE_EMPRUNT).FirstOrDefault();
+            chargerDataGrid(2);
 
             // US6 remove abonnes qui n'ont pas empruntés depuis un an
             var empruntsExpires = Outils.musique.EMPRUNTER
@@ -76,14 +78,27 @@ namespace Dedisclasik
             var abonnesExpires = Outils.musique.EMPRUNTER
                 .Where(a => dateNow.Year - a.DATE_EMPRUNT.Year > 0)
                 .Select(a => a.ABONNÉS).ToList();
-            Outils.musique.EMPRUNTER.RemoveRange(empruntsExpires);
-            Outils.musique.ABONNÉS.RemoveRange(abonnesExpires);
-            Outils.musique.SaveChanges();
+            foreach(ABONNÉS a in abonnesExpires)
+            {
+                string[] row = new string[] { a.NOM_ABONNÉ, a.PRÉNOM_ABONNÉ };
+                dataGridView1.Rows.Add(row);
+            }
+
+            if (!afficherMessageVide())
+            {
+                var confirmResult = MessageBox.Show("Êtes-vous sûr de vouloir tout supprimer ?", "Purger", MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+                    Outils.musique.EMPRUNTER.RemoveRange(empruntsExpires);
+                    Outils.musique.ABONNÉS.RemoveRange(abonnesExpires);
+                    Outils.musique.SaveChanges();
+                }
+            }
         }
 
         private void albumsNonEmprunts_Click(object sender, EventArgs e)
         {
-            listInfos.Items.Clear();
+            chargerDataGrid(1);
             DateTime dateNow = DateTime.Now;
 
             //  US8 : liste albums non empruntés depuis + d'un an 
@@ -92,10 +107,53 @@ namespace Dedisclasik
             var emprunteurs = Outils.musique.EMPRUNTER
                 .Where(a => dateNow.Year - a.DATE_EMPRUNT.Year > 0)
                 .Select(a => a.ALBUMS).ToList();
-            foreach (ALBUMS a in emprunteurs)
+            foreach (ALBUMS a in emprunteurs) dataGridView1.Rows.Add(a.TITRE_ALBUM);
+            afficherMessageVide();
+        }
+
+        private void chargerDataGrid(int nbColonnes)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = nbColonnes;
+            if (nbColonnes == 1)
             {
-                listInfos.Items.Add(a.TITRE_ALBUM);
+                dataGridView1.Columns[0].Name = "Titre";
+                dataGridView1.Columns[0].Width = dataGridView1.Width;
             }
+            else if (nbColonnes == 2)
+            {
+                dataGridView1.Columns[0].Name = "Nom";
+                dataGridView1.Columns[0].Width = dataGridView1.Width / 2;
+                dataGridView1.Columns[1].Name = "Prénom";
+                dataGridView1.Columns[1].Width = dataGridView1.Width / 2;
+            }
+            else
+            {
+                dataGridView1.Columns[0].Name = "Titre";
+                dataGridView1.Columns[0].Width = dataGridView1.Width / 2;
+                dataGridView1.Columns[1].Name = "Nom";
+                dataGridView1.Columns[1].Width = dataGridView1.Width / 5;
+                dataGridView1.Columns[2].Name = "Prénom";
+                dataGridView1.Columns[2].Width = dataGridView1.Width / 5;
+            }
+        }
+
+        private bool afficherMessageVide()
+        {
+            bool estVide = false;
+            if (dataGridView1.Rows.Count == 0)
+            {
+                MessageBox.Show("Liste vide.");
+                estVide = true;
+            }
+            return estVide;
+        }
+
+        private void deconnect_Click(object sender, EventArgs e)
+        {
+            var confirmResult = MessageBox.Show("Etes-vous sûr ?", "Déconnexion", MessageBoxButtons.YesNo);
+            if (confirmResult == DialogResult.Yes) Close();
         }
     }
 }
