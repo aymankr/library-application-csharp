05fa1b2a2496025ae3bfb87c20db1ed667991eae
Ayman Kr <kr.ayman@hotmail.com>
2021-06-08 14:03:30 +0200

    IHM administrateur en cours

--- projet/Dedisclasik/Dedisclasik/Administrateur.Designer.cs
+++ projet/Dedisclasik/Dedisclasik/Administrateur.Designer.cs
@@ -37,12 +37,13 @@ namespace Dedisclasik
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.deconnect = new System.Windows.Forms.Button();
+            this.sousTitre = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // empruntProlong
             // 
-            this.empruntProlong.Location = new System.Drawing.Point(12, 490);
+            this.empruntProlong.Location = new System.Drawing.Point(12, 546);
             this.empruntProlong.Name = "empruntProlong";
             this.empruntProlong.Size = new System.Drawing.Size(154, 49);
             this.empruntProlong.TabIndex = 0;
@@ -53,41 +54,41 @@ namespace Dedisclasik
             // purger
             // 
             this.purger.BackColor = System.Drawing.Color.LightCoral;
-            this.purger.Location = new System.Drawing.Point(738, 490);
+            this.purger.Location = new System.Drawing.Point(738, 546);
             this.purger.Name = "purger";
             this.purger.Size = new System.Drawing.Size(154, 49);
             this.purger.TabIndex = 1;
-            this.purger.Text = "PURGER ABONNÉS N\'AYANT PAS EMPRUNTÉ DEPUIS UN AN";
+            this.purger.Text = "PURGER ABONNÉS";
             this.purger.UseVisualStyleBackColor = false;
             this.purger.Click += new System.EventHandler(this.purger_Click);
             // 
             // empruntRetard
             // 
-            this.empruntRetard.Location = new System.Drawing.Point(172, 490);
+            this.empruntRetard.Location = new System.Drawing.Point(172, 546);
             this.empruntRetard.Name = "empruntRetard";
             this.empruntRetard.Size = new System.Drawing.Size(154, 49);
             this.empruntRetard.TabIndex = 2;
-            this.empruntRetard.Text = "EMPRUNTS EN RETARD DE 10 JOURS";
+            this.empruntRetard.Text = "EMPRUNTS EN RETARD";
             this.empruntRetard.UseVisualStyleBackColor = true;
             this.empruntRetard.Click += new System.EventHandler(this.empruntRetard_Click);
             // 
             // empruntMeilleurs
             // 
-            this.empruntMeilleurs.Location = new System.Drawing.Point(332, 490);
+            this.empruntMeilleurs.Location = new System.Drawing.Point(332, 546);
             this.empruntMeilleurs.Name = "empruntMeilleurs";
             this.empruntMeilleurs.Size = new System.Drawing.Size(154, 49);
             this.empruntMeilleurs.TabIndex = 3;
-            this.empruntMeilleurs.Text = "10 ALBUMS LES PLUS EMPRUNTÉS DE L\'ANNEE";
+            this.empruntMeilleurs.Text = "TOP 10 ALBUMS";
             this.empruntMeilleurs.UseVisualStyleBackColor = true;
             this.empruntMeilleurs.Click += new System.EventHandler(this.empruntMeilleurs_Click);
             // 
             // albumsNonEmprunts
             // 
-            this.albumsNonEmprunts.Location = new System.Drawing.Point(492, 490);
+            this.albumsNonEmprunts.Location = new System.Drawing.Point(492, 546);
             this.albumsNonEmprunts.Name = "albumsNonEmprunts";
             this.albumsNonEmprunts.Size = new System.Drawing.Size(154, 49);
             this.albumsNonEmprunts.TabIndex = 5;
-            this.albumsNonEmprunts.Text = "ALBUMS NON EMPRUNTÉS DEPUIS UN AN";
+            this.albumsNonEmprunts.Text = "ALBUMS NON EMPRUNTÉS";
             this.albumsNonEmprunts.UseVisualStyleBackColor = true;
             this.albumsNonEmprunts.Click += new System.EventHandler(this.albumsNonEmprunts_Click);
             // 
@@ -98,17 +99,17 @@ namespace Dedisclasik
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 52);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 91);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(880, 432);
+            this.dataGridView1.Size = new System.Drawing.Size(880, 449);
             this.dataGridView1.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(6, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(330, 36);
             this.label1.TabIndex = 8;
@@ -128,11 +129,22 @@ namespace Dedisclasik
             this.deconnect.UseVisualStyleBackColor = false;
             this.deconnect.Click += new System.EventHandler(this.deconnect_Click);
             // 
+            // sousTitre
+            // 
+            this.sousTitre.AutoSize = true;
+            this.sousTitre.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sousTitre.Location = new System.Drawing.Point(12, 58);
+            this.sousTitre.Name = "sousTitre";
+            this.sousTitre.Size = new System.Drawing.Size(204, 19);
+            this.sousTitre.TabIndex = 10;
+            this.sousTitre.Text = "Veuillez sélectionner une option";
+            // 
             // Administrateur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(904, 551);
+            this.ClientSize = new System.Drawing.Size(904, 607);
+            this.Controls.Add(this.sousTitre);
             this.Controls.Add(this.deconnect);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -160,5 +172,6 @@ namespace Dedisclasik
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button deconnect;
+        private System.Windows.Forms.Label sousTitre;
     }
 }
