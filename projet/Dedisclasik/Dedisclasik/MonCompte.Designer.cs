6f7df9df3a38a020156cb200ecc9f9be57bb2ad2
vjosso <vjosso@INFO-206-21>
2021-06-08 14:10:09 +0200

    test

--- projet/Dedisclasik/Dedisclasik/MonCompte.Designer.cs
+++ projet/Dedisclasik/Dedisclasik/MonCompte.Designer.cs
@@ -41,6 +41,8 @@ namespace Dedisclasik
             this.prenomUtilisateur = new System.Windows.Forms.Label();
             this.loginUtilisateur = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // albumEmprunt
@@ -156,11 +158,20 @@ namespace Dedisclasik
             this.label6.TabIndex = 11;
             this.label6.Text = "Mon compte";
             // 
-            // InterfaceTemp
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(23, 453);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(751, 235);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // MonCompte
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 720);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.loginUtilisateur);
             this.Controls.Add(this.prenomUtilisateur);
@@ -173,8 +184,9 @@ namespace Dedisclasik
             this.Controls.Add(this.dateRetourAttendue);
             this.Controls.Add(this.Prolonger);
             this.Controls.Add(this.albumEmprunt);
-            this.Name = "InterfaceTemp";
+            this.Name = "MonCompte";
             this.Text = "InterfaceTemp";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -194,5 +206,6 @@ namespace Dedisclasik
         private System.Windows.Forms.Label prenomUtilisateur;
         private System.Windows.Forms.Label loginUtilisateur;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
