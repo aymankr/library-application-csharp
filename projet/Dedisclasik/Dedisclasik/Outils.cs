c8bb20675c2a48ea6cddf9f2dd03a81e33355561
Bailhet Raphael <raphael.bailhet@etu.u-bordeaux.fr>
2021-06-10 10:17:35 +0200

    ajout dans outils de pagination meme si y'a une adaptation à refaire (appels)

--- projet/Dedisclasik/Dedisclasik/Outils.cs
+++ projet/Dedisclasik/Dedisclasik/Outils.cs
@@ -10,6 +10,9 @@ namespace Dedisclasik
     {
         public static int pgNb = 1;
         public static int pgSz = 15;
+        public static String fonction = "";
+        public static List<string> actions = new List<string>();
+        public static int cptActions = 0;
         public static MusiquePT2_NEntities musique { get; set; }
 
         public static bool dejaProlongé(EMPRUNTER emprunt)
@@ -37,6 +40,30 @@ namespace Dedisclasik
                 dg.Columns[i].Width = dg.Width / nbColonnes;
                 i++;
                 
+            }
+        }
+
+        public static void activePaging(int nbMax, System.Windows.Forms.Button btPrec, System.Windows.Forms.Button btNext, System.Windows.Forms.Label lab )
+        {
+            btPrec.Enabled = true;
+            btNext.Enabled = true;
+            lab.Text = "Page : " + pgNb.ToString() + "/" + (nbMax / pgSz + 1).ToString();
+            if (pgNb <= 1)
+            {
+                btPrec.Enabled = false;
+            }
+            if (pgNb >= (int)(nbMax / pgSz) + 1)
+            {
+                btNext.Enabled = false;
+            }
+        }
+        public static void comparer()
+        {
+            actions.Add(fonction);
+            cptActions++;
+            if (!(actions[cptActions].Equals(actions[cptActions - 1])))
+            {
+                pgNb = 1;
             }
         }
 
