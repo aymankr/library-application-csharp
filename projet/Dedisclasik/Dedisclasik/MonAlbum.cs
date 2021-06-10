19f46fbd5e8e5864d39ffb12cf386a8d4c5f8c42
dgavlak <damien.gavlak@etu.u-bordeaux.fr>
2021-06-10 13:08:41 +0200

    pochetteOK

--- projet/Dedisclasik/Dedisclasik/MonAlbum.cs
+++ projet/Dedisclasik/Dedisclasik/MonAlbum.cs
@@ -8,44 +8,6 @@ namespace Dedisclasik
 {
     public partial class ALBUMS
     {
-        /*override
-        public string ToString()
-        {
-            /*string nonPrécisé = "Non précisée";
-            string date = nonPrécisé;
-            if (this.ANNÉE_ALBUM != null)
-            {
-                date = this.ANNÉE_ALBUM.ToString().Trim();
-            }
-            string editeur = nonPrécisé;
-            string pays = nonPrécisé;
-            if (this.EDITEURS != null)
-            {
-                editeur = this.EDITEURS.NOM_EDITEUR.Trim();
-                if (this.EDITEURS.PAYS != null)
-                {
-                    pays = this.EDITEURS.PAYS.NOM_PAYS.Trim();
-                }
-            }
-            string genre = nonPrécisé;
-            if (this.GENRES != null)
-            {
-                genre = this.GENRES.LIBELLÉ_GENRE.Trim();
-            }
-            string infos = this.TITRE_ALBUM.Trim() + "   " + editeur + "   " + date + "   " + pays + "   " + genre;
-            if (Outils.dejaEmprunté(this))
-            {
-                infos += " -> Déjà emprunté";
-            }
-            return infos;*/
-
-        /*string infos = this.TITRE_ALBUM.Trim();
-        if (Outils.dejaEmprunté(this))
-        {
-            infos += " -> Déjà emprunté";
-        }
-        return infos;
-    }*/
 
         public string getEditeur()
         {
